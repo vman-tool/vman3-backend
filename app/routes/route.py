04d75c6ef27d8133e4d2d66b0b1d3d09bc159ff3
jonas1015 <jonasgsmart1015@gmail.com>
2024-06-10 17:30:45 +0300

    Add a feature branch to download by chunks

--- app/routes/route.py
+++ app/routes/route.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter, HTTPException, status
 from loguru import logger
 
 from app.configs.database import db
-from app.utilits.odk_client import ODKClient
+from app.utilits.utils import ODKClient
+import json
+from pandas import json_normalize
 
 odk_router = APIRouter(
     prefix="/odk",
@@ -18,8 +20,8 @@ odk_router = APIRouter(
 async def fetch_and_store_data(
     start_date: str = None, 
     end_date: str = None,
-    skip: int = 0,
-    top: int = 1000,
+    skip: int = 10,
+    top: int = 10,
 ):
     if start_date is None:
         start_date = '2021-01-01'  # Replace with your desired default start date
@@ -28,12 +30,29 @@ async def fetch_and_store_data(
             
     logger.info(f"Fetching data from ODK between {start_date} and {end_date}")
     with ODKClient() as odk_client:
-        data = odk_client.get_form_submissions(start_date, end_date, top=top, skip=skip)
+        if skip is None:
+            pass
+        data = odk_client.getFormSubmissions(start_date, end_date, top=top, skip=skip)
+
+        total_data_count = data["@odata.count"]
+
+
+        while (skip < total_data_count):
+            percent = total_data_count / 100
+            print("Get more data")
+
+            skip += top
+
+        json_flattened = json.loads(json_normalize(data=data['value'], sep='/').to_json(orient='records'))
+        with open("flattened.json", 'w') as file:
+            print(total_data_count)
+            json.dump(json_flattened, file)
+        
 
     if isinstance(data, str):
         logger.error(f"Error fetching data: {data}")
         raise HTTPException(status_code=500, detail=data)
-    collection = db["form_submissions"]
-    await collection.insert_many(data['value'])
+    # collection = db["form_submissions"]
+    # await collection.insert_many(json_flattened)
     logger.info(f"Data inserted successfully: {len(data['value'])} records")
     return {"status": "Data inserted successfully"}
