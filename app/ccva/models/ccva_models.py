6a624daefe341b0a18dc0509a309b995b40d8584
crisskimaryo <crisskimaryo@gmail.com>
2024-09-05 06:29:20 +0300

    fix ccva filter by date

--- app/ccva/models/ccva_models.py
+++ app/ccva/models/ccva_models.py
@@ -1,10 +1,13 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class InterVA5Progress(BaseModel):
-    progress: int
+    total_records: Optional[int] = 0
+    progress: Optional[int] = 0
     message: str
     status: str
-    elapsed_time: str
+    elapsed_time: Optional[str] = "0:00:00"
     task_id: str
     error: bool
