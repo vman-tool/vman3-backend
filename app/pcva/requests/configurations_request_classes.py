f9221e0eb17eff9a67a41c41c12dda07022202f0
Jonas1015 <jonasgsmart1015@gmail.com>
2026-03-27 18:45:19 +0300

    Add option to pcva configurations to allow other coders work to be shown in pcva

--- app/pcva/requests/configurations_request_classes.py
+++ app/pcva/requests/configurations_request_classes.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
+from typing import Union
 
 
 class PCVAConfigurationsRequest(BaseModel):
     useICD11: bool
     vaAssignmentLimit: int
     concordanceLevel: int
+    showOtherCodersWork: Union[bool, None] = None
